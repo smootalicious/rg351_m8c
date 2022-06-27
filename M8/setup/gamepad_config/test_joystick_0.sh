444b414333522605e0d8f9addef3c00095263306
smootalicious <smootalicious@gmail.com>
2022-06-27 17:45:30 -0500

    Update config gamepad scripts to output to both current screen and device screen

--- M8/setup/gamepad_config/test_joystick_0.sh
+++ M8/setup/gamepad_config/test_joystick_0.sh
@@ -2,4 +2,6 @@
 CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $CURRENT_PATH
 
-sudo chmod 666 /dev/tty1; ./gamepad_config -j 0 > /dev/tty1; printf "\033c" >> /dev/tty1;
+sudo chmod 666 /dev/tty1;
+./gamepad_config -j 0 | tee /dev/tty1;
+printf "\033c" >> /dev/tty1;
